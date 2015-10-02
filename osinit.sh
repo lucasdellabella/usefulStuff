6f7621124fd18d55d69b1841167486219ebdfd44
Lucas Della Bella <ipodgamedev@gmail.com>
2015-10-01 20:32:58 -0400

    Update awesome config in osinit.sh

--- osinit.sh
+++ osinit.sh
@@ -7,12 +7,13 @@ echo "--git"
 echo "--emacs"
 echo "--leiningen"
 echo "--awesome window manager"
+echo "--timeout, unzip utils"
 
 sudo mkdir $HOME/dev
-cd   $HOME/dev
 
-echo "install timeout"
+echo "install utilities"
 sudo apt-get install timeout
+sudo apt-get install unzip
 
 # git is probably already installed
 echo "install git"
@@ -31,3 +32,12 @@ $HOME/dev/usefulStuff/getlein.ba
 echo "install awesome"
 sudo apt-get install awesome
 cp $HOME/dev/usefulStuff/rcgeneric.lua /etc/xdg/awesome/rc.lua
+
+#should def a var
+sudo unzip -d$HOME/.config/awesome $HOME/dev/usefulStuff/awesome-copycats-master.zip
+sudo unzip -d$HOME/.config/awesome/awesome-copycats-master.zip $HOME/dev/usefulStuff/lain-master.zip
+
+# Enable bitmapped fonts?
+## cd /etc/fonts/conf.d/
+## sudo rm /etc/fonts/conf.d/10* && sudo rm -rf 70-no-bitmaps.conf && sudo ln -s ../conf.avail/70-yes-bitmaps.conf .
+## sudo dpkg-reconfigure fontconfig
