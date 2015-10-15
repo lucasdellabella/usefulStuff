86ab040dfef9b64925d13e96411f12d0a53fd86e
Lucas Della Bella <ipodgamedev@gmail.com>
2015-10-15 01:13:15 -0400

    Create table of contents. Add bg img items. Add .sh script to switch bgs. Updates.

--- bootup.sh
+++ bootup.sh
@@ -9,5 +9,6 @@
 ### END INIT INFO
 
 xrandr --output DP1 --mode 2560x1440 --right-of eDP1
-redshift -l 33.769146899999996:-84.39136429999999
 xinput --set-prop "Razer Razer DeathAdder" "Device Accel Constant Deceleration" 5
+redshift -l 33.769146899999996:-84.39136429999999
+
